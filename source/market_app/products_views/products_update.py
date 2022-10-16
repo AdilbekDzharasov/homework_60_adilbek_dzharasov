1dc1a69f16b973b76ee4c4dc1f0c72554679a320
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-16 16:54:16 +0600

    Изменен CRUD для обновления товара на классовое представление UpdateView

--- source/market_app/products_views/products_update.py
+++ source/market_app/products_views/products_update.py
@@ -1,31 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from market_app.models.product import Product
+from django.urls import reverse
+from django.views.generic import UpdateView
 from market_app.forms import ProductForm
+from market_app.models import Product
 
 
-def update_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'GET':
-        form = ProductForm(initial={
-            'name': product.name,
-            'description': product.description,
-            'image': product.image,
-            'categories': product.categories,
-            'remainder': product.remainder,
-            'price': product.price
-        })
-        return render(request, 'product_update.html', context={'form': form, 'product': product})
-    elif request.method == 'POST':
-        form = ProductForm(data=request.POST)
-        if form.is_valid():
-            product.name = form.cleaned_data['name']
-            product.description = form.cleaned_data['description']
-            product.image = form.cleaned_data['image']
-            product.categories = form.cleaned_data['categories']
-            product.remainder = form.cleaned_data['remainder']
-            product.price = form.cleaned_data['price']
-            product.save()
-            return redirect('product_detail', pk=product.pk)
-        else:
-            return render(request, 'product_update.html', context={'form': form, 'product': product})
+class ProductUpdateView(UpdateView):
+    template_name = 'products/product_update.html'
+    form_class = ProductForm
+    model = Product
+    context_object_name = 'product'
+
+    def get_success_url(self):
+        return reverse('home')
 
--- source/market_app/urls.py
+++ source/market_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from market_app.products_views.products_delete import delete_view
-from market_app.products_views.products_update import update_view
 
 from market_app.products_views.base import HomeView
 from market_app.products_views.products_detail import ProductDetailView
 from market_app.products_views.products_add import ProductCreateView
+from market_app.products_views.products_update import ProductUpdateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('products/add/', ProductCreateView.as_view(), name='product_add'),
     path('products/delete/<int:pk>', delete_view, name='product_delete'),
-    path('products/update/<int:pk>', update_view, name='product_update')
+    path('products/update/<int:pk>', ProductUpdateView.as_view(), name='product_update')
 ]
 

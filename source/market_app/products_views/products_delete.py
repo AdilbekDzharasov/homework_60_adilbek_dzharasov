fe1818b6b75806573c18290ae041d37556a28b83
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-16 17:03:42 +0600

    Изменен CRUD для удаления товара на DeleteView в products_delete.py, изменен путь в urls.py, внесены корретровки в product_delete.html

--- source/market_app/products_views/products_delete.py
+++ source/market_app/products_views/products_delete.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from market_app.models.product import Product
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
+from market_app.models import Product
 
 
-def delete_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'GET':
-        return render(request, 'product_delete.html', context={'product': product})
-    elif request.method == 'POST':
-        product.delete()
-        return redirect('home')
+class ProductDeleteView(DeleteView):
+    template_name = 'products/product_delete.html'
+    model = Product
+    context_object_name = 'product'
+    success_url = reverse_lazy('home')
 
--- source/market_app/urls.py
+++ source/market_app/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from market_app.products_views.products_delete import delete_view
 
 from market_app.products_views.base import HomeView
 from market_app.products_views.products_detail import ProductDetailView
 from market_app.products_views.products_add import ProductCreateView
+from market_app.products_views.products_delete import ProductDeleteView
 from market_app.products_views.products_update import ProductUpdateView
+
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('products/add/', ProductCreateView.as_view(), name='product_add'),
-    path('products/delete/<int:pk>', delete_view, name='product_delete'),
+    path('products/delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
     path('products/update/<int:pk>', ProductUpdateView.as_view(), name='product_update')
 ]
 
